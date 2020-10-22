9b012086f9eb2cd3fbfa976cf3735439dfa655e7
j3ffh95 <j3ffh95@gmail.com>
2020-10-22 17:17:34 -0400

    Created function isInViewport

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 const items = document.querySelectorAll("#timeline li");
+
+const inInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
